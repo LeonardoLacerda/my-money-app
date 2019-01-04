a49c9469f81ca447ae2929cf1bcba6e7b8f85b91
Leonardo Lacerda <leonardolacerda@tce.al.gov.br>
2019-01-04 16:43:32 -0300

    Criação dos componentes e integração com redux
    Signed-off-by: Leonardo Lacerda <leonardolacerda@tce.al.gov.br>

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; 2019
+            <a href="https://www.tce.al.gov.br" target="_blank"> TCEAL</a>
+        </strong>
+    </footer>
+)
